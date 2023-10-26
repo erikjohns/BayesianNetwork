6b8bb907e3bdcd833eded1c70e25bc8ca7486b13
Johns, Erik Michael <erik.johns@student.tugraz.at>
2023-10-25 19:53:58 -0600

    Added Scenarios and a Test Scenario

--- src/Driver.java
+++ src/Driver.java
@@ -1,15 +1,36 @@
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Driver {
 
     public static void main(String[] args) {
         String[] fileNames = {"alarm.bif", "child.bif", "hailfinder.bif", "insurance.bif", "win95pts.bif"};
 
-        FileParser parser = new FileParser("./src/Networks/child.bif");
+        FileParser parser = new FileParser("./src/Networks/alarm.bif");
         Network network = parser.parseBIF();
 
+        HashMap<String, String> littleEvidence = new HashMap<>();
+        littleEvidence.put("HRBP", "HIGH");
+        littleEvidence.put("CO", "LOW");
+        littleEvidence.put("BP", "HIGH");
+
+        HashMap<String, String> moderateEvidence = new HashMap<>();
+        moderateEvidence.put("HRBP", "HIGH");
+        moderateEvidence.put("CO", "LOW");
+        moderateEvidence.put("BP", "HIGH");
+        moderateEvidence.put("HRSAT", "LOW");
+        moderateEvidence.put("HREKG", "LOW");
+        moderateEvidence.put("HISTORY", "TRUE");
+
+        List<String> query = Arrays.asList("Disease");
+
+        Scenario lowEvidenceScenario = new Scenario(littleEvidence, query);
+        Scenario moderateEvidenceScenario = new Scenario(moderateEvidence, query);
+
         network.printNetwork();
+
+
 
     }
 }
--- src/Scenario.java
+++ src/Scenario.java
@@ -1,2 +1,27 @@
+import java.util.HashMap;
+import java.util.List;
+
 public class Scenario {
+    HashMap<String, String> evidence;
+    List<String> query;
+
+    // Constructor for no evidence
+    public Scenario(List<String> query) {
+        this.evidence = new HashMap<>();
+        this.query = query;
+    }
+
+    // Constructor for evidence
+    public Scenario(HashMap<String, String> evidence, List<String> query) {
+        this.evidence = evidence;
+        this.query = query;
+    }
+
+    public HashMap<String, String> getEvidence() {
+        return evidence;
+    }
+
+    public List<String> getQuery() {
+        return query;
+    }
 }
