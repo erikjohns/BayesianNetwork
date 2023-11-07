c2aaa4410f7fec00a8b7ad880114f310b1b73fc6
Johns, Erik Michael <erik.johns@student.tugraz.at>
2023-11-06 22:14:29 -0700

    Run All Files

--- src/Driver.java
+++ src/Driver.java
@@ -13,13 +13,10 @@ public class Driver {
     public static void main(String[] args) {
 
         runAlarm();
-        /*
         runChild();
         runHailfinder();
         runInsurance();
         runWin95Pts();
-
-         */
 
     }
 
